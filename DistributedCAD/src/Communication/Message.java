14bd2adb5705fe4ec231a36f66de84163871d0ac
Johan Häggmark <johan.haggmark90@gmail.com>
2019-02-26 22:28:21 +0100

    Sender RTTMessageRepeater Ska vara redo att användas

--- DistributedCAD/src/Communication/Message.java
+++ DistributedCAD/src/Communication/Message.java
@@ -5,10 +5,11 @@ import se.his.drts.message.Envelope;
 public class Message {
 
 	private Envelope envelope;
-	private String id;
+	private int id;
 	private int attempt = 0;
+	public boolean isAcknowledge = false;
 
-	public Message(String id, Envelope envelope) {
+	public Message(int id, Envelope envelope) {
 		this.envelope = envelope;
 		this.id = id;
 	}
@@ -17,11 +18,14 @@ public class Message {
 		return envelope;
 	}
 	
-	public String getId() {
+	public int getId() {
 		return id;
 	}
 
 	public int getAttempt() {
 		return attempt;
+	}
+	public void incrementAttempt() {
+		attempt++;
 	}
 }
--- DistributedCAD/src/Communication/Messages.java
+++ DistributedCAD/src/Communication/Messages.java
@@ -13,16 +13,29 @@ import se.his.drts.message.MessagePayload.IncorrectMessageException;
 
 public class Messages {
 	private LinkedBlockingQueue m_messageQueue;
+	private LinkedBlockingQueue m_RTTMessageQueue;
+	//private ConcurrentHasMap Skapa hashmappen som håller alla meddelanden
+	int id = 0;
 	
 	public Messages() {
 		m_messageQueue = new LinkedBlockingQueue<Message>();
+		m_RTTMessageQueue = new LinkedBlockingQueue<Message>();
 	}
 	
-	public void addToMessageQueue(GObject obj) {
-		m_messageQueue.add(convertObjectToMessage(obj));
+	
+	public void addNewMessage(GObject obj) {
+		addToMessageQueue(new Message(id, convertObjectToEnvelope(obj)));
+		id++;
 	}
 	
-	private Envelope convertObjectToMessage(GObject obj) {
+	public void addToMessageQueue(Message msg) {
+		m_messageQueue.add(convertObjectToEnvelope(msg));
+	}
+	public void addToRTTMessageQueue(Message msg) {
+		m_RTTMessageQueue.add(convertObjectToEnvelope(msg));
+	}
+	
+	private Envelope convertObjectToEnvelope(Object obj) {
 		ObjectMapper om = new ObjectMapper();
 		byte[] b;
 		try {
@@ -39,7 +52,28 @@ public class Messages {
 		return null;
 	}
 	
+	/*private Envelope convertObjectToEnvelope(GObject obj) {
+		ObjectMapper om = new ObjectMapper();
+		byte[] b;
+		try {
+			b = om.writeValueAsBytes(obj);
+			
+			return Envelope.createEnvelope(b);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IncorrectMessageException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}*/
+	
 	public LinkedBlockingQueue getMessageQueue() {
 		return m_messageQueue;
+	}
+	
+	public LinkedBlockingQueue getRTTMessageQueue() {
+		return m_RTTMessageQueue;
 	}
 }
