d0d0f14ba1be294a020a6cd1361a73edf7577a0d
Alfred <Alfred@Alfred-PC>
2019-03-07 11:53:12 +0100

    Tog bort execute från payload

--- DistributedMessageSerialization/src/se/his/drts/message/AbstractMessageTopClass.java
+++ DistributedMessageSerialization/src/se/his/drts/message/AbstractMessageTopClass.java
@@ -10,70 +10,6 @@ public class AbstractMessageTopClass extends MessagePayload {
 	private static final long serialVersionUID = 1L;
 
 	
-	public static class MessageIdentity implements Comparable<MessageIdentity> {
-		private UUID uuid;
-		private BigInteger subIdentity;
-
-		public MessageIdentity(UUID uuid2, BigInteger subIdentity2) {
-			this.uuid = uuid2;
-			this.subIdentity = subIdentity2;
-		}
-
-		@Override
-		public int compareTo(MessageIdentity arg0) {
-			final int n = this.uuid.compareTo(arg0.uuid);
-			if (n != 0) {
-				return n;
-			}
-			return this.subIdentity.compareTo(arg0.subIdentity);
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see java.lang.Object#equals(java.lang.Object)
-		 */
-		@Override
-		public boolean equals(Object obj) {
-			final MessageIdentity mi = (MessageIdentity) obj;
-			return this.uuid.equals(mi.uuid) && this.subIdentity.equals(mi.subIdentity);
-		}
-
-		/**
-		 * @return the identity
-		 */
-		public final BigInteger getSubIdentity() {
-			return subIdentity;
-		}
-
-		/**
-		 * @return the uuid
-		 */
-		public final UUID getUuid() {
-			return uuid;
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see java.lang.Object#hashCode()
-		 */
-		@Override
-		public int hashCode() {
-			return this.uuid.hashCode() + this.subIdentity.hashCode();
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see java.lang.Object#toString()
-		 */
-		@Override
-		public String toString() {
-			return "MessageIdentity [uuid=" + uuid + ", subIdentity=" + subIdentity + "]";
-		}
-
-	}
 	private int attempt = 0;
 
 	private static BigInteger nextSubIdentity = BigInteger.ONE;
@@ -96,7 +32,6 @@ public class AbstractMessageTopClass extends MessagePayload {
 		}
 	}
 
-	@Override
 	public void executeInClient() {
 
 	}
