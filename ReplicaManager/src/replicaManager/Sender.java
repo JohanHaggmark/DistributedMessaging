2f8fa79b323164ce8bdd39cb9e6a3b4bf32b3ad5
Alfred <Alfred@Alfred-PC>
2019-03-15 14:50:28 +0100

    ack

--- ReplicaManager/src/replicaManager/Sender.java
+++ ReplicaManager/src/replicaManager/Sender.java
@@ -29,7 +29,7 @@ public class Sender implements Runnable {
 				LocalMessage msg = (LocalMessage) m_messages.getMessageQueue().take();
 				JGroups.logger.debugLog("Sender trying to send to: " + msg.getMsgTopClass().getName());
 				JGroups.logger.debugLog("Sender found a message in m_messages");
-				if (!msg.isAcknowledge && msg.getAttempt() < ATTEMPTS) {
+				if (!msg.isAcknowledged() && msg.getAttempt() < ATTEMPTS) {
 					JGroups.logger.debugLog("Sending - Sender 32");
 					m_channel.send(new Message(null, msg.getMsgTopClass()));
 					if (msg.getMsgTopClass().getUUID().toString().equals("54f642d7-eaf6-4d62-ad2d-316e4b821c03")) {
